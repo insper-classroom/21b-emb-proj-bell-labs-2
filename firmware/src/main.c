4947b7a62a40ad90c8ea3c0592d3b1a54d29939c
Diego Saragoza da Silva <diegosaragozaa2@gmail.com>
2021-10-19 11:05:44 -0300

    Bluetooth task

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,4 +1,5 @@
 #include <asf.h>
+#include <string.h>
 #include "conf_board.h"
 #include <stdlib.h>
 
@@ -16,7 +17,7 @@
 #define BUT_PIO_PIN			11
 #define BUT_PIO_PIN_MASK	(1 << BUT_PIO_PIN)
 
-/* define DEBUG_SERIAL */
+#define DEBUG_SERIAL
 
 #ifdef DEBUG_SERIAL
 #define USART_COM USART1
@@ -29,6 +30,9 @@
 /* RTOS  */
 #define TASK_LCD_STACK_SIZE					(1024/sizeof(portSTACK_TYPE))
 #define TASK_LCD_STACK_PRIORITY	            (tskIDLE_PRIORITY)
+
+#define TASK_BLUETOOTH_STACK_SIZE            (4096/sizeof(portSTACK_TYPE))
+#define TASK_BLUETOOTH_STACK_PRIORITY        (tskIDLE_PRIORITY)
 
 /* AFEC */
 #define AFEC_POT AFEC0
@@ -69,6 +73,7 @@ extern void vApplicationMallocFailedHook(void) {
 volatile uint32_t g_sdram_cnt = 0 ;
 uint32_t *g_sdram = (uint32_t *)BOARD_SDRAM_ADDR;
 volatile char but_flag = 0;
+volatile int bluetooth_init = 0;
 
 void but_callback(void) {
 	but_flag = 1;
@@ -181,6 +186,72 @@ static void config_AFEC_pot(Afec *afec, uint32_t afec_id, uint32_t afec_channel,
 	NVIC_EnableIRQ(afec_id);
 }
 
+uint32_t usart_puts(uint8_t *pstring) {
+	uint32_t i ;
+
+	while(*(pstring + i))
+	if(uart_is_tx_empty(USART_COM))
+	usart_serial_putchar(USART_COM, *(pstring+i++));
+}
+
+void usart_put_string(Usart *usart, char str[]) {
+	usart_serial_write_packet(usart, str, strlen(str));
+}
+
+int usart_get_string(Usart *usart, char buffer[], int bufferlen, uint timeout_ms) {
+	uint timecounter = timeout_ms;
+	uint32_t rx;
+	uint32_t counter = 0;
+
+	while( (timecounter > 0) && (counter < bufferlen - 1)) {
+		if(usart_read(usart, &rx) == 0) {
+			buffer[counter++] = rx;
+		}
+		else{
+			timecounter--;
+			vTaskDelay(1);
+		}
+	}
+	buffer[counter] = 0x00;
+	return counter;
+}
+
+void usart_send_command(Usart *usart, char buffer_rx[], int bufferlen,
+char buffer_tx[], int timeout) {
+	usart_put_string(usart, buffer_tx);
+	usart_get_string(usart, buffer_rx, bufferlen, timeout);
+}
+
+void config_usart0(void) {
+	sysclk_enable_peripheral_clock(ID_USART0);
+	usart_serial_options_t config;
+	config.baudrate = 9600;
+	config.charlength = US_MR_CHRL_8_BIT;
+	config.paritytype = US_MR_PAR_NO;
+	config.stopbits = false;
+	usart_serial_init(USART0, &config);
+	usart_enable_tx(USART0);
+	usart_enable_rx(USART0);
+
+	// RX - PB0  TX - PB1
+	pio_configure(PIOB, PIO_PERIPH_C, (1 << 0), PIO_DEFAULT);
+	pio_configure(PIOB, PIO_PERIPH_C, (1 << 1), PIO_DEFAULT);
+}
+
+int hc05_init(void) {
+	char buffer_rx[128];
+	usart_send_command(USART_COM, buffer_rx, 1000, "AT", 100);
+	vTaskDelay( 500 / portTICK_PERIOD_MS);
+	usart_send_command(USART_COM, buffer_rx, 1000, "AT", 100);
+	vTaskDelay( 500 / portTICK_PERIOD_MS);
+	usart_send_command(USART_COM, buffer_rx, 1000, "AT+NAMEFeijoada", 100);
+	vTaskDelay( 500 / portTICK_PERIOD_MS);
+	usart_send_command(USART_COM, buffer_rx, 1000, "AT", 100);
+	vTaskDelay( 500 / portTICK_PERIOD_MS);
+	usart_send_command(USART_COM, buffer_rx, 1000, "AT+PIN4321", 100);
+	vTaskDelay( 500 / portTICK_PERIOD_MS);
+}
+
 /************************************************************************/
 /* TASKS                                                                */
 /************************************************************************/
@@ -210,6 +281,33 @@ void task_adc(void){
 			vTaskDelay(500 / portTICK_PERIOD_MS);
 			afec_enable_interrupt(AFEC_POT, AFEC_POT_CHANNEL);
 		}
+	}
+}
+
+void task_bluetooth(void) {
+	printf("Task Bluetooth started \n");
+	printf("Inicializando HC05 \n");
+	config_usart0();
+	hc05_init();
+	
+	char button1 = '0';
+	char eof = 'X';
+
+	// Task não deve retornar.
+	while(1) {
+		// envia status botão
+		while(!usart_is_tx_ready(USART_COM)) {
+			vTaskDelay(10 / portTICK_PERIOD_MS);
+		}
+		usart_write(USART_COM, button1);
+		
+		// envia fim de pacote
+		while(!usart_is_tx_ready(USART_COM)) {
+			vTaskDelay(10 / portTICK_PERIOD_MS);
+		}
+		usart_write(USART_COM, eof);
+
+		vTaskDelay(500 / portTICK_PERIOD_MS);
 	}
 }
 
@@ -281,9 +379,14 @@ int main(void) {
 	init();
 
 	/* Create task to handler LCD */
+	if (xTaskCreate(task_bluetooth, "BLT", TASK_BLUETOOTH_STACK_SIZE, NULL,	TASK_BLUETOOTH_STACK_PRIORITY, NULL) != pdPASS) {
+		printf("Failed to create test adc task\r\n");
+	}
+	
 	if (xTaskCreate(task_adc, "adc", TASK_LCD_STACK_SIZE, NULL, TASK_LCD_STACK_PRIORITY, NULL) != pdPASS) {
 		printf("Failed to create test adc task\r\n");
 	}
+	
 
 	/* Start the scheduler. */
 	vTaskStartScheduler();
